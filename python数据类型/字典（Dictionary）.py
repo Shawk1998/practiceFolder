7b16dfc1c547ee2f7227b98477c474bcc63b85ca
Shawk1998 <2017161047@email.szu.edu.cn>
2021-07-27 10:31:40 +0800

    字典

--- python数据类型/字典（Dictionary）.py
+++ python数据类型/字典（Dictionary）.py
@@ -55,13 +55,16 @@
 
 # 字典视图
 # 调用一下的方法返回对象
-# 1.keys返回字典所有key的视图
-# 2.values返回字典所有value的视图
-# 3.items返回所有k-v视图
+# 1.keys返回字典所有key的视图 --> dict_keys(['k1','k2'...])
+# 2.values返回字典所有value的视图 --> dict_values(['v1','v2'...])
+# 3.items返回所有k-v视图 --> dict_items([('k1','v1'),('k2','v2')...])
 # 视图会随字典变化而变化
 
-
-
-
-
-
+# 使用字典创建格式化字符串
+# 1.百分号作为占位符
+# print('aaaa %s, bbbb %s' % (dictName['k1'], dictName['k2']))
+# 打印时会将k1,k2对应的v1,v2会依次塞到前两个百分号中
+# 或者print('aaaa %(k1)s, bbbb %(k2)s' % dictName) 可以自定义传入位置
+# 2.使用花括号作为占位符
+# print('aaaa {}, bbbb {}' .format(dictName['k1'], dictName['k2']))
+# print('aaaa {k1}, bbbb {k2}' .format_map(dictName))
